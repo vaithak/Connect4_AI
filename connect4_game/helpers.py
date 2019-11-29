a3b9dec7dc0769c3111ac6c9b36feba90464feb6
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2019-11-29 16:19:38 +0000

    Add function for detecting if game finished

--- connect4_game/helpers.py
+++ connect4_game/helpers.py
@@ -15,7 +15,35 @@ def verify_board_state_difference(active, prev_board_state, state):
 
 # check if game finished
 def game_finished(user_state):
-	return True
+	num_of_rows    = 6
+	num_of_columns = 7
+	connect_num	   = 4
+
+	# check all rows
+	for row_num in range(num_of_rows):
+		for index in range(num_of_columns - connect_num + 1):
+			if (user_state[row_num*num_of_columns + index] != '0') and (user_state[row_num*num_of_columns + index] == user_state[row_num*num_of_columns + index + 1]) and (user_state[row_num*num_of_columns + index + 1] == user_state[row_num*num_of_columns + index + 2]) and (user_state[row_num*num_of_columns + index + 2] == user_state[row_num*num_of_columns + index + 3]):
+				return True
+
+	# check all columns
+	for column_num in range(num_of_columns):
+		for index in range(num_of_rows - connect_num + 1):
+			if (user_state[index*num_of_columns + column_num] != '0') and (user_state[index*num_of_columns + column_num] == user_state[(index+1)*num_of_columns + column_num]) and (user_state[(index+1)*num_of_columns + column_num] == user_state[(index+2)*num_of_columns + column_num]) and (user_state[(index+2)*num_of_columns + column_num] == user_state[(index+3)*num_of_columns + column_num]):
+				return True
+
+	# check right downward diagonals
+	for row_num in range(num_of_rows - connect_num + 1):
+		for column_num in range(num_of_columns - connect_num + 1):
+			if (user_state[row_num*num_of_columns + column_num] != '0') and (user_state[row_num*num_of_columns + column_num] == user_state[(row_num+1)*num_of_columns + column_num + 1]) and (user_state[(row_num+1)*num_of_columns + column_num + 1] == user_state[(row_num+2)*num_of_columns + column_num + 2]) and (user_state[(row_num + 2)*num_of_columns + column_num + 2] == user_state[(row_num + 3)*num_of_columns + column_num + 3]):
+				return True
+
+	# check left downward diagonals
+	for row_num in range(num_of_rows - connect_num + 1):
+		for column_num in range(connect_num-1, num_of_columns):
+			if (user_state[row_num*num_of_columns + column_num] != '0') and (user_state[row_num*num_of_columns + column_num] == user_state[(row_num+1)*num_of_columns + column_num - 1]) and (user_state[(row_num+1)*num_of_columns + column_num - 1] == user_state[(row_num + 2)*num_of_columns + column_num - 2]) and (user_state[(row_num + 2)*num_of_columns + column_num - 2] == user_state[(row_num + 3)*num_of_columns + column_num - 3]):
+				return True
+
+	return False
 
 # TODO
 def ai_move(curr_active, curr_state):
