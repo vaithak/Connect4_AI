22da11a4f60a3256e89708c7ab4deb4427e531ee
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2019-11-29 08:36:22 +0000

    Add production settings

--- Connect4_AI/settings.py
+++ Connect4_AI/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v%qz@bor=3%dbi0xfwm&p!90thberx9vm4bv7&g15icm#c#g4f'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'SOME+RANDOM+BACKUPKEz9+3vnmjb0u@&w68t#5_e8s9-lbfhv-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE', True)
 
-ALLOWED_HOSTS = ['18.217.104.66', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['18.217.104.66', 'localhost', '127.0.0.1', 'con-4.herokuapp.com']
 
 
 # Application definition
@@ -130,3 +130,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: daphne Connect4_AI.asgi:application --port $PORT --bind 0.0.0.0
